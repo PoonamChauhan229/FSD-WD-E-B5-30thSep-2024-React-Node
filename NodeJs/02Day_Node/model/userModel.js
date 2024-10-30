f7db1787d81f6081ca9c9c73a5e3bc72378cf262
PoonamChauhan229 <poonam.chauhan229@gmail.com>
2024-10-30 12:31:21 +0530

    mongodb atlas

--- NodeJs/02Day_Node/model/userModel.js
+++ NodeJs/02Day_Node/model/userModel.js
@@ -1,9 +1,52 @@
 const mongoose=require('mongoose')
-const User=mongoose.model('User',{
+const validator=require('validator')
+const userSchema={
     // define your schema
-    name:String,
-    age:Number,
-    email:String,
-    phoneNumber:String
-})
+    name:{
+        type:String,
+        required:true
+    },
+    age:{
+        type:Number,
+        required:true,
+        validate(value){
+            if(value<0){
+                throw new Error("Age must be a +ve integer")
+            }
+        },
+        default:0
+    },
+    email:{
+        type:String,
+        lowercase:true,
+        trim:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Email Invalid")
+            }
+
+        }
+    },
+    phoneNumber:{
+        type: String,
+        validate(value){
+          if(!validator.isMobilePhone(value)){
+            throw new Error("invalid Phone number")
+          }
+        }
+      },
+      password:{
+        type: String,
+        required:true,
+        validate(value){
+          if(value.toLowerCase().includes("password")){
+            throw new Error("Your Password shouldnt contain password!!!")
+          }
+          else if(!validator.isStrongPassword(value)){
+            throw new Error("put strong password")
+          }
+        }
+      }
+}
+const User=mongoose.model('User',userSchema)
 module.exports=User
--- NodeJs/02Day_Node/routes/userRoutes.js
+++ NodeJs/02Day_Node/routes/userRoutes.js
@@ -2,18 +2,33 @@ const User=require('../model/userModel')
 const express=require('express')
 const router=express.Router()
 
+//error comes >> handle out properly 
+// try catch || if else 
 // post methods >>
 router.post('/adduser',async(req,res)=>{
-    //console.log(req.body)
-    const newUser=new User(req.body)
-    await newUser.save()
-    res.send(newUser)
+    try{
+        //console.log(req.body)
+        const newUser=new User(req.body)
+        await newUser.save()
+        res.status(200).send(newUser)
+    }
+    catch(e){
+        res.status(500).send({"message":e})
+    }
 })
 
-//getAll User
+//getAll User  >> try catch || if else 
 router.get('/users',async(req,res)=>{
-   const getAllUser=await User.find({})
-   res.send(getAllUser)
+   try{
+    const getAllUser=await User.find({})
+    if(!getAllUser){
+       return  res.status(400).send({"message":"Users Not Avialable"})// early return
+    }
+     res.status(200).send(getAllUser)
+   }
+   catch(e){
+    res.status(500).send({"messagae":"Some Internal error"})
+   }
 })
 
 //get a single user >> retrive :id 
@@ -21,8 +36,18 @@ router.get('/users',async(req,res)=>{
 router.get('/users/:id',async(req,res)=>{
     console.log(req.params.id)
     // let getUser=await User.find({_id:req.params.id})
-    let getUser=await User.findById(req.params.id)
-    res.send(getUser)
+    try{
+        console.log(req.params.id)
+        // let getUser=await User.find({_id:req.params.id})
+        let getUser=await User.findById(req.params.id)
+        if(!getUser){
+            return res.status(400).send({"message": "User Not found"})
+        }
+        res.status(200).send(getUser)
+        }
+        catch(e){
+            res.status(500).send({"message":"Some Internal Error"})
+        }
 })
 
 //update
